83d14d1282385268942cdc3492d76069fdc2237d
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-12-27 20:19:16 -0800

    implement edit functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,7 @@
 import { FaCheckCircle, FaEdit } from "react-icons/fa";
 import type { Todo } from "../Common/model";
 import { MdDelete } from "react-icons/md";
+import { useState } from "react";
 
 type SingleTodoProps = {
   todo: Todo;
@@ -9,6 +10,9 @@ type SingleTodoProps = {
 };
 
 export const SingleTodo = ({ todo, todos, setTodos }: SingleTodoProps) => {
+
+    const [edit,setEdit] = useState<boolean>(false) 
+    const [editTodo,setEditTodo] = useState<string>(todo.todo) 
 
     // handle  Done Function
 
@@ -27,10 +31,24 @@ export const SingleTodo = ({ todo, todos, setTodos }: SingleTodoProps) => {
     setTodos(todos.filter(todo => todo.id != id))
   }
 
+
+//   edit function
+
+const handleEdit =(e:React.FormEvent,id:number) => {
+    //  if(!todo.)
+     e.preventDefault();
+     setTodos(todos.map(todo => todo.id == id ? {...todo,todo:editTodo } : todo))
+     setEdit(false)
+}
+ 
   return (
-    <form>
+    <form onSubmit={(e) => handleEdit(e,todo.id)}>
       <div className="bg-cyan-400 text-white rounded-md flex justify-between items-center px-4 py-3">
-        {todo.isDone ? (
+   {
+     edit ? (
+            <input value={editTodo} type="text" className="border border-white text-black px-2 py-1 rounded-md" onChange={(e) => setEditTodo(e.target.value) } />
+     ) : <>
+               {todo.isDone ? (
           <>
             <s className="capitalize"> {todo.todo} </s>
           </>
@@ -39,9 +57,18 @@ export const SingleTodo = ({ todo, todos, setTodos }: SingleTodoProps) => {
             <span className="capitalize"> {todo.todo} </span>
           </>
         )}
+  </>
+      
+   }
 
         <div className="action_icons flex items-center gap-3 ">
-          <div className="text-xl cursor-pointer">
+          <div className="text-xl cursor-pointer"
+          onClick={() => {
+              if(!todo.isDone){
+                setEdit(true)
+              }
+          }}
+          >
             {" "}
             <FaEdit />{" "}
           </div>
