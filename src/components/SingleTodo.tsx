406617260de50d7c79c2d57ad148a26d6a0f1656
Md Shorif Uddin <mdshorifuddin463@gmail.com>
2025-12-27 19:09:10 -0800

    implement isDone Functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,27 +1,63 @@
 import { FaCheckCircle, FaEdit } from "react-icons/fa";
-import type { Todo } from "../Common/model"
+import type { Todo } from "../Common/model";
 import { MdDelete } from "react-icons/md";
 
 type SingleTodoProps = {
-    todo : Todo;
-     todos : Todo[];
-    setTodos : React.Dispatch<React.SetStateAction<Todo[]>>
-}
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-export const SingleTodo = ({todo,setTodos}:SingleTodoProps) => {
+export const SingleTodo = ({ todo, todos, setTodos }: SingleTodoProps) => {
+
+    // handle  Done Function
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id == id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+
+//  delete function
+
+  const handleDelete = (id : number) => {
+
+  }
+
   return (
     <form>
+      <div className="bg-cyan-400 text-white rounded-md flex justify-between items-center px-4 py-3">
+        {todo.isDone ? (
+          <>
+            <s className="capitalize"> {todo.todo} </s>
+          </>
+        ) : (
+          <>
+            <span className="capitalize"> {todo.todo} </span>
+          </>
+        )}
 
-             <div className="bg-cyan-400 text-white rounded-md flex justify-between items-center px-4 py-3">
-                  <span className="capitalize"> {todo.todo} </span>
-
-                    <div className="action_icons flex items-center gap-3 ">
-                          <div className="text-xl cursor-pointer"> <FaEdit/> </div>
-                          <div className="text-xl cursor-pointer"> <MdDelete/> </div>
-                          <div className="text-xl cursor-pointer"> <FaCheckCircle/> </div>
-                    </div>
-             </div>
-
+        <div className="action_icons flex items-center gap-3 ">
+          <div className="text-xl cursor-pointer">
+            {" "}
+            <FaEdit />{" "}
+          </div>
+          <div className="text-xl cursor-pointer">
+            {" "}
+            <MdDelete />{" "}
+          </div>
+          <div
+            className="text-xl cursor-pointer"
+            onClick={() => handleDone(todo.id)}
+          >
+            {" "}
+            <FaCheckCircle />{" "}
+          </div>
+        </div>
+      </div>
     </form>
-  )
-}
+  );
+};
